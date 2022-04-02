89630c60b15c2cbf9c274230e1c1d7205874442d
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-04-02 19:31:37 +0900

    hello world

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string aFriend="Bill";
+Console.WriteLine(aFriend);
+aFriend = "Maira";
+Console.WriteLine(aFriend);
+Console.WriteLine("Hello" + aFriend);
+Console.WriteLine($"Hello {aFriend}");
