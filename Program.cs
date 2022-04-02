3697c5309dc5afa1cebc28f78a2d5fb7dbde21c3
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-04-02 20:27:57 +0900

    hello world 3

--- Program.cs
+++ Program.cs
@@ -28,3 +28,11 @@ sayHello = sayHello.Replace("Hello", "Greetings");
 Console.WriteLine(sayHello);
 Console.WriteLine(sayHello.ToUpper());
 Console.WriteLine(sayHello.ToLower());
+//  hello world 3
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
+Console.WriteLine(songLyrics.StartsWith("You"));
+Console.WriteLine(songLyrics.StartsWith("goodbye"));
+Console.WriteLine(songLyrics.EndsWith("goodbye"));
+Console.WriteLine(songLyrics.EndsWith("hello"));
