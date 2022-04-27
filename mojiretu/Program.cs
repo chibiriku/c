9ebbfd32f570288f7c1d61dca7997dabb77e69ff
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-04-27 20:38:12 +0900

    0427

--- mojiretu/Program.cs
+++ mojiretu/Program.cs
@@ -5,4 +5,10 @@ double c = 8;
 double d = (a + b) / c;
 Console.WriteLine(d);
 
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
 
