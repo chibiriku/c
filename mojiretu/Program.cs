0c02ff85ee37a997ee99890602aca8c738e4a199
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-05-02 20:15:04 +0900

    0502

--- mojiretu/Program.cs
+++ mojiretu/Program.cs
@@ -1,18 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-decimal min = decimal.MinValue;
-decimal max = decimal.MaxValue;
-Console.WriteLine($"The range of the decimal type is {min} to {max}");
+﻿// データを受け取って表示しよう
+using System;
 
-double a = 1.0;
-double b = 3.0;
-Console.WriteLine(a / b);
-
-decimal c = 1.0M;
-decimal d = 3.0M;
-Console.WriteLine(c / d);
-
-double radius = 2.50;
-double area = Math.PI * radius * radius;
-Console.WriteLine(area);
-// 円の面積公式
+class Program
+{
+    static void Main()
+    {
+        var name = Console.ReadLine().Trim();
+        Console.WriteLine(name);
+    }
+}
 
