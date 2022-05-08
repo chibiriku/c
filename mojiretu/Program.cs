853600999480679d9c6455ccf0a604ad3782ed75
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-05-08 20:00:39 +0900

    0508

--- mojiretu/Program.cs
+++ mojiretu/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//特定の条件で数値を表示
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var numberString = Console.ReadLine().Trim();
+        var number = int.Parse(numberString);
+        if(number >= 10)
+        {
+            Console.WriteLine(number);
+        }
+
+    }
+}
